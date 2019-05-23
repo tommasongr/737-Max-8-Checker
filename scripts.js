f1b0dcfcf02b7a254294d8ea24b98a9110bd3075
Tommaso Negri <tommaso.negri98@gmail.com>
2019-05-23 17:49:08 +0200

    Logic

---
+++ scripts.js
@@ -0,0 +1,18 @@
+var request = new XMLHttpRequest()
+
+console.log(valueAPI)
+request.open('GET', valueAPI, true)
+request.onload = function() {
+  // Begin accessing JSON data here
+  var data = JSON.parse(this.response)
+
+  if (request.status >= 200 && request.status < 400) {
+    data.forEach(movie => {
+      console.log(movie.title)
+    })
+  } else {
+    console.log('error')
+  }
+}
+
+request.send()
